569440bafe9ee24748048b5b87c9487b945818e4
hiev <145098693+ikuyooo@users.noreply.github.com>
2025-08-22 10:38:39 +0000

    add project code

--- Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        tet nguoi = new tet("Nguyen Van A", 20, "nguyenvana@gmail.com");
-        nguoi.HienThiThongTin();
-    }
-}
