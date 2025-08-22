c951bbd58820ebf4c7eec84454661c51136104b2
CongDau358 <148378615+CongDau358@users.noreply.github.com>
2025-08-22 10:06:56 +0000

    commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        tet nguoi = new tet("Nguyen Van A", 20, "nguyenvana@gmail.com");
+        nguoi.HienThiThongTin();
+    }
+}
