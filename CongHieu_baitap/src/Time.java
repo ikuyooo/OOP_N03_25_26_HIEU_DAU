d2c0ab95517c08c14fe82388ee39c26006970821
hiev <145098693+ikuyooo@users.noreply.github.com>
2025-09-05 10:23:20 +0000

    baitap

--- CongHieu_baitap/src/Time.java
+++ CongHieu_baitap/src/Time.java
@@ -1,29 +1,67 @@
 public class Time {
-    int hour;
-    int minute;
-    int second;
+    private int hour;
+    private int minute;
+    private int second;
 
-    public Time(int hour, int minute, int second){
-        this.hour = hour;
-        this.minute = minute;
-        this.second = second;
+    public Time() {
+        setTime(0, 0, 0);
     }
-    public int getHour(){
-        return this.hour;
+
+    public Time(int h) {
+        setTime(h, 0, 0);
     }
-    public void setHour(int hour){
-        this.hour = hour;
+
+    public Time(int h, int m) {
+        setTime(h, m, 0);
     }
-    public int getMinute(){
-        return this.minute;
+
+    public Time(int h, int m, int s) {
+        setTime(h, m, s);
     }
-    public void setMinute(int minute){
-        this.minute = minute;
+
+    public void setTime(int h, int m, int s) {
+        setHour(h);
+        setMinute(m);
+        setSecond(s);
     }
-    public int getSecond(){
-        return this.second;
+
+    public int getHour() {
+        return hour;
     }
-    public void setSecond(int second){
-        this.second = second;
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int h) {
+        if (h >= 0 && h < 24) {
+            this.hour = h;
+        } else {
+            this.hour = 0;
+        }
+    }
+
+    public void setMinute(int m) {
+        if (m >= 0 && m < 60) {
+            this.minute = m;
+        } else {
+            this.minute = 0;
+        }
+    }
+
+    public void setSecond(int s) {
+        if (s >= 0 && s < 60) {
+            this.second = s;
+        } else {
+            this.second = 0;
+        }
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hour, minute, second);
     }
 }
