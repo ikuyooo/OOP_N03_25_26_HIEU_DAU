1e05008e506bc2b4ea1937d6358f4232aef1d4ee
hiev <145098693+ikuyooo@users.noreply.github.com>
2025-09-05 09:54:44 +0000

    baitaptuan3

---
+++ CongHieu_baitap/src/Time.java
@@ -0,0 +1,29 @@
+public class Time {
+    int hour;
+    int minute;
+    int second;
+
+    public Time(int hour, int minute, int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    public int getHour(){
+        return this.hour;
+    }
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+    public int getMinute(){
+        return this.minute;
+    }
+    public void setMinute(int minute){
+        this.minute = minute;
+    }
+    public int getSecond(){
+        return this.second;
+    }
+    public void setSecond(int second){
+        this.second = second;
+    }
+}
