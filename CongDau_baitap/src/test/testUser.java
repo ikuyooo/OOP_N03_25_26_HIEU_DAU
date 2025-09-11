c413c56261f75844df40362fb2a642cd02c95b65
CongDau358 <148378615+CongDau358@users.noreply.github.com>
2025-09-11 13:31:55 +0700

    Delete CongDau_baitap directory

--- CongDau_baitap/src/test/testUser.java
+++
@@ -1,9 +0,0 @@
-package test;
-import src.User;
-
-public class testUser {
-     public static void run() {
-        User u1 = new User("CongDau", 20,"23010863 ","KHMT","HaNoi");
-        System.out.println(u1);
-    }
-}
