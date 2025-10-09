9eb85dc9bc8c4744f332020c161c6ec674121ac0
CongDau358 <148378615+CongDau358@users.noreply.github.com>
2025-10-09 07:35:25 +0000

    update

---
+++ quanlykhachsan/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO room (room_number, room_type, price, status)
+VALUES 
+('101', 'Standard', 300000, 'Available'),
+('102', 'Deluxe', 500000, 'Occupied'),
+('103', 'Suite', 800000, 'Available');
