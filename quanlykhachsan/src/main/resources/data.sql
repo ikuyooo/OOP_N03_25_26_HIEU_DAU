164c3f253f15e882349c240044e33e481763929b
CongDau358 <148378615+CongDau358@users.noreply.github.com>
2025-10-16 13:30:01 +0700

    Delete quanlykhachsan directory

--- quanlykhachsan/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO room (room_number, room_type, price, status)
-VALUES 
-('101', 'Standard', 300000, 'Available'),
-('102', 'Deluxe', 500000, 'Occupied'),
-('103', 'Suite', 800000, 'Available');
