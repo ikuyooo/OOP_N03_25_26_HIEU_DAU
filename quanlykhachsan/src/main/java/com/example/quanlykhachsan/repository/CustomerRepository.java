22120f1b85e1f74a0e61b95593a357eed5185101
hiev <145098693+ikuyooo@users.noreply.github.com>
2025-11-07 15:53:26 +0700

    Delete quanlykhachsan directory

--- quanlykhachsan/src/main/java/com/example/quanlykhachsan/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.quanlykhachsan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.quanlykhachsan.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
