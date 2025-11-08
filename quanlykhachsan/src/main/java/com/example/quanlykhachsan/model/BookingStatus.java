6faabe28c0a5e85a9f416529e871bd74474f1ac2
hiev <145098693+ikuyooo@users.noreply.github.com>
2025-11-08 17:57:10 +0000

    update booking status

---
+++ quanlykhachsan/src/main/java/com/example/quanlykhachsan/model/BookingStatus.java
@@ -0,0 +1,14 @@
+package com.example.quanlykhachsan.model;
+
+public enum BookingStatus {
+    
+    PENDING,
+    
+    CONFIRMED,
+    
+    CHECKED_IN,
+    
+    CHECKED_OUT,
+    
+    CANCELLED
+}
