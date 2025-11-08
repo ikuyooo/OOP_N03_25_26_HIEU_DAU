b3924b545d4890de49bf0225f0d99fb9125cc0d1
hiev <145098693+ikuyooo@users.noreply.github.com>
2025-11-08 18:02:46 +0000

    update dto

---
+++ quanlykhachsan/src/main/java/com/example/quanlykhachsan/dto/BookingResponseDto.java
@@ -0,0 +1,81 @@
+package com.example.quanlykhachsan.dto;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class BookingResponseDto {
+    private int bookingId;
+    private String customerName;
+    private String roomName;
+    private Date checkInDate;
+    private Date checkOutDate;
+    private BigDecimal totalPrice;
+    private String status;
+    private Timestamp createdAt;
+
+    //Getter & Setter
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
