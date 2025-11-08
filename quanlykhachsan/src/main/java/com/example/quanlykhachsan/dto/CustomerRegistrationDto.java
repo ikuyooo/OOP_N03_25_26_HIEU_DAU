c913d6096a549bc0afb08c766696335fdb9d15f1
hiev <145098693+ikuyooo@users.noreply.github.com>
2025-11-08 18:04:25 +0000

    update dto

---
+++ quanlykhachsan/src/main/java/com/example/quanlykhachsan/dto/CustomerRegistrationDto.java
@@ -0,0 +1,70 @@
+package com.example.quanlykhachsan.dto;
+
+public class CustomerRegistrationDto {
+    // Từ User
+    private String username;
+    private String password;
+
+    // Từ Customer
+    private String name;
+    private String phone;
+    private String email;
+    private String idCard;
+    private String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
